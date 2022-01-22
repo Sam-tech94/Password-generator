4fb7523e145270f65cb060f5f9ba0bddc451948d
Sam Petrosyan <81108029+Sam-tech94@users.noreply.github.com>
2022-01-22 23:03:03 +0400

    Add files via upload

---
+++ Password-generator/main.js
@@ -0,0 +1,44 @@
+const empty = "",
+    uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lCase = "abcdefghijklmnopqrstuvwxyz",
+    number = "0123456789",
+    symbol = "!@#$%^&*=-_";
+
+const pLength = document.getElementById("p-length"),
+    uppercase = document.getElementById("p-uppercase"),
+    lowercase = document.getElementById("p-lowercase"),
+    pNumber = document.getElementById("p-number"),
+    pSymbol = document.getElementById("p-symbol"),
+    submit = document.getElementById("submit"),
+    password = document.getElementById("password"),
+    copy = document.getElementById("copy");
+    
+submit.addEventListener("click", () => {
+    let initialPassword = empty;
+
+    uppercase.checked ? (initialPassword += uCase) : "";
+    lowercase.checked ? (initialPassword += lCase) : "";
+    pNumber.checked ? (initialPassword += number) : "";
+    pSymbol.checked ? (initialPassword += symbol) : "";
+
+    password.value = generatePassword(pLength.value, initialPassword);
+});    
+
+function generatePassword(l, initialPassword) {
+    let pass = "";
+    for (let i = 0; i < l; i++) {
+        pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
+    }
+
+    return pass;
+}
+
+copy.addEventListener("click", () => {
+    if (password.value === "") {
+        alert("Please generate a password");
+    } else {
+        password.select();
+        document.execCommand("copy");
+        alert("Password has been copied to clipboard");
+    }
+});
